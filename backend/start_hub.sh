3da2f8670927e51b3bea1e02524d69f738e27aab
AdamBear <adamxiong@126.com>
2022-11-28 11:03:44 +0800

    use hub ocr version

--- backend/start_hub.sh
+++ backend/start_hub.sh
@@ -1,2 +1,3 @@
-export CUDA_VISIBLE_DEVICES=0
-nohup hub serving start -c hub_serving/ocr_system/config.json > ocr.log 2>&1 & echo $! > pid_ocr.txt
+#export CUDA_VISIBLE_DEVICES=0
+#nohup hub serving start -c hub_serving/ocr_system/config.json > ocr.log 2>&1 & echo $! > pid_ocr.txt
+nohup hub serving start -m chinese_ocr_db_crnn_server -p 8866 > ocr.log 2>&1 & echo $! > pid_ocr.txt
